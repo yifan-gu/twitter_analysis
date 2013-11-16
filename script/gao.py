89555f86a6caf3995ee3a1552275b2c05b4918af
Hongchao Deng <fengjingchao@gmail.com>
2013-11-15 20:07:50 -0500

    finish parsing json file

--- script/gao.py
+++ script/gao.py
@@ -5,8 +5,45 @@ import subprocess
 import json
 import datetime
 import calendar
+import struct
 
-def cao(tweet_json, output):
+ts_output = open('hb_ts', "w")
+ud_output = open('hb_uid', "w")
+rt_output = open('hb_rt', "w")
+
+def load(tid, uid, ts, tx, rt):
+
+    def barr2str(arr):
+        return "".join("\\x{:02X}".format(ord(i)) for i in arr)
+
+    ts_output.write(
+            # put timestamp, tweet_ts, tweet:tw_id, text
+            'put "timestamp", "{}", "tweet:{}","{}"\n'.format(
+                barr2str(struct.pack(">Q", ts)),
+                barr2str(struct.pack(">Q", tid)),
+                tx)
+            )
+    ud_output.write(
+            # put user, uid, tweet:tw_id, ""
+            'put "user", "{}", "tweet:{}",""\n'.format(
+                barr2str(struct.pack(">Q", uid)),
+                barr2str(struct.pack(">Q", tid)) )
+            )
+
+    if rt == "":
+        return
+
+    rt_uid = int(rt)
+
+    rt_output.write(
+            # put retweet, rt_uid, tweet:tw_id, uid
+            'put "retweet", "{}", "tweet:{}","{}"\n'.format(
+                barr2str(struct.pack(">Q", rt_uid)),
+                barr2str(struct.pack(">Q", tid)),
+                barr2str(struct.pack(">Q", uid)) )
+            )
+
+def cao(tweet_json):
     tweet = json.loads(tweet_json)
     # tweet id
     # user id
@@ -17,30 +54,44 @@ def cao(tweet_json, output):
         "%a %b %d %X %Y").utctimetuple()
         )
     # text
-    output.write( u"{}\t{}\t{}\t{}\t{}\n".format(
-                        tweet['id_str'].zfill(20),
-                        tweet['user']['id_str'].zfill(20),
-                        tweet['retweeted_status']['user']['id_str'].zfill(20) if tweet.has_key('retweeted_status') else "",
-                        ts,
-                        tweet['text']
-                    ).encode('utf8')
-                )
+    text = tweet['text']
+    #print "".join("\\x{:02X}".format(ord(i)) for i in text.encode('utf8'))
+    text = "".join("\\x{:02X}".format(ord(i)) for i in text.encode('utf8'))
+    #sp = (tweet['text'].encode('utf8')
+                      #.replace('\\','\\\\')
+                      #.replace('"', '\\"')
+                      #.split('\n') )
+    #text = "\\n".join(sp)
+
+    #output.write( u"{}\t{}\t{}\t{}\t{}\n".format(
+                        #tweet['id_str'],
+                        #tweet['user']['id_str'],
+                        #tweet['retweeted_status']['user']['id_str'] if tweet.has_key('retweeted_status') else "",
+                        #ts,
+                        #text.decode('utf8')
+                    #).encode('utf8')
+                #)
+    load(   int(tweet['id_str'] ),
+            int(tweet['user']['id_str'] ),
+            int(ts ),
+            text,
+            tweet['retweeted_status']['user']['id_str'] if tweet.has_key('retweeted_status') else ""
+        )
 
 
 def gao(s3_path):
 
     filename = os.path.basename(s3_path)
-    subprocess.check_call(
-            "s3cmd get {0}".format(s3_path),
-            shell=True
-            )
-    with open(filename+"_output", "w") as output:
-        with open(filename, "r") as f:
-            for line in f:
-                if line.rstrip() != "":
-                    cao( line.rstrip() , output)
+    #subprocess.check_call(
+            #"s3cmd get {0}".format(s3_path),
+            #shell=True
+            #)
+    with open(filename, "r") as f:
+        for line in f:
+            if line.rstrip() != "":
+                cao( line.rstrip() )
 
-    os.remove(filename )
+    #os.remove(filename )
 
 
 with open("./json_files", "r") as f:
